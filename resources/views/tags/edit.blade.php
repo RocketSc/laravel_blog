a4f4e4f275e70c58506a2fd0e0bbbad70f5584fc
RocketScience <rocketscience.webmaster@gmail.com>
2017-04-03 16:43:15 +0300

    Creating tags.edit view

---
+++ resources/views/tags/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('main')
+
+@section('title', 'Edit tag')
+
+@section('content')
+    <form action="{{ route('tags.update', $tag) }}" method="POST">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input class="form-control" id="name" type="text" value="{{ $tag->name }}">
+        </div>
+        
+        <div class="form-group">
+            <input class="btn btn-success" type="submit" value="Save">
+        </div>
+    </form>
+@endsection  
