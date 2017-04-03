38fa863346eacfdd745d83e3365899e2d54036da
RocketScience <rocketscience.webmaster@gmail.com>
2017-04-03 16:27:47 +0300

    Creating tags.show view

---
+++ resources/views/tags/show.blade.php
@@ -0,0 +1,38 @@
+@extends('main')
+
+@section('title', $tag->name . ' tag' )
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $tag->name }} Tag <small>{{ $tag->posts()->count() }} Posts</small></h1>
+        </div>
+
+        <div class="col-md-2 col-md-offset-2">
+            <a class="btn btn-primary btn-block pull-right" href="{{ route('tags.edit', $tag) }}">Edit</a>
+        </div>
+    </div>
+
+    <div class="row col-md-12">
+        <table class="table">
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Tags</th>
+                <th></th>
+            </tr>
+            @foreach($tag->posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td>
+                        @foreach($post->tags as $tag)
+                            <span class="label label-default">{{ $tag->name }}</span>
+                        @endforeach
+                    </td>
+                    <td><a href="{{ route('posts.show', $post) }}" class="btn btn-default btn-sm">View</a></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+@endsection
