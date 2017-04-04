317fcca987d6d9a6a42cddd4c620388308aff03d
RocketScience <rocketscience.webmaster@gmail.com>
2017-04-04 12:15:17 +0300

    Fixing error in  in Comment model

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['name', 'email', 'comment'];
+    protected $fillable = ['name', 'email', 'body'];
 
     public function post()
     {
