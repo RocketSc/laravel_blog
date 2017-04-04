c27e78e375e82d68d50dfa44c227f2b4cc1a4a90
RocketScience <rocketscience.webmaster@gmail.com>
2017-04-04 11:52:39 +0300

    Creating comment form in blog.single view

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -11,4 +11,34 @@
             <p>Posted in: {{ $post->category->name }}</p>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset2">
+            <form action="{{route('comment.store', $post)}}"
+                  class="form"
+                  method="POST">
+
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="name">Name: </label>
+                    <input class="form-control" type="text" id="name" name="name">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email: </label>
+                    <input class="form-control" id="email" type="email" name="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="comment">Your comment: </label>
+                    <textarea class="form-control" name="comment" id="comment" cols="30" rows="10"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <input class="btn btn-success" type="submit" value="Add Comment">
+                </div>
+
+            </form>
+        </div>
+    </div>
 @endsection
