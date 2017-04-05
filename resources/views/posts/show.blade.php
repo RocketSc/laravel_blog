9f3c4c81a21685f847332417aa816d9b21267b93
RocketScience <rocketscience.webmaster@gmail.com>
2017-04-05 14:29:23 +0300

    Adding image display to posts.show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-8">
+        <img src="{{ asset('images/' . $post->image) }}" alt="">
         <h1>{{ $post->title }}</h1>
 
         <div class="tags">
